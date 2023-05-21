49478f11ed035e3b18f0b1b1f6fc7b5ef44cc1c6
helenserb <serbinenkoifbb@gmail.com>
2023-05-21 17:21:40 -0400

    add invokeAction func

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 const contacts = require("./contacts")
-contacts.listContacts()
+const argv = require("yargs").argv;
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+        const allContact = contacts.listContacts();
+        return console.log(allContact);
+
+    case "get":
+        const oneContact = contacts.getContactById(id);
+        return console.log(oneContact)
+
+    case "add":
+          const newContact = contacts.addContact({ name, email, phone });
+          return console.log(newContact);
+
+
+    case "remove":
+        const deletedContact = contacts.removeContact(id);
+        return console.log(deletedContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
