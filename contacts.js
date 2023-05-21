6eb82b0a9fdc9b4021857d09084ea8aef2d865fd
helenserb <serbinenkoifbb@gmail.com>
2023-05-21 16:28:18 -0400

    add listContacts func

--- contacts.js
+++ contacts.js
@@ -1,2 +1,29 @@
 const fs = require("fs/promises");
 
+/*
+ * Розкоментуйте і запиши значення
+ * const contactsPath = ;
+ */
+
+// TODO: задокументувати кожну функцію
+const listContacts = async() => {
+    const data = await fs.readFile("./db/contacts.json", "utf-8");
+    return console.log(JSON.parse(data))
+  };
+
+
+// const getContactById = async(contactId)=> {
+//   // ...твій код
+// }
+
+// function removeContact(contactId) {
+//   // ...твій код
+// }
+
+// const addContact = async(name, email, phone) => {
+//   // ...твій код
+// }
+module.exports = {
+    listContacts,
+    // getContactById, removeContact, addContact
+};
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-console.log("hello world")
+const contacts = require("./contacts")
+contacts.listContacts()
